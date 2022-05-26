eaf26d6758383b33b4e4a4aa62cab42056471fe4
TridentMark <2411kjn@gmail.com>
2022-05-26 18:54:02 +0700

    feat: add thread

--- src/com/company/ThreadClient.java
+++ src/com/company/ThreadClient.java
@@ -1,11 +1,21 @@
 package com.company;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+
 public class ThreadClient {
+
+    static void runThreadClient(PrintWriter out, String text) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            out.println(text);
+        });
+        t1.start();
+    }
+
     public static void main(String[] args) throws IOException {
         final String HOST = "127.0.0.1";
         final int PORT = 4040;
@@ -17,14 +27,17 @@ public class ThreadClient {
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 Scanner in = new Scanner(socket.getInputStream());
                 Scanner s = new Scanner(System.in);
+
         ) {
+            runThreadClient(out, "Run thread client 1");
+            runThreadClient(out, "Run thread client 2");
+            Thread.sleep(1000);
+
             while (true) {
-                System.out.print("Input: ");
-                String input = s.nextLine();
-                out.println(input);
-                if (input.equalsIgnoreCase("exit")) break;
                 System.out.println("Echoed from server: " + in.nextLine());
             }
+        } catch (InterruptedException e) {
+            System.out.println("Client interrupted.");
         }
     }
 
